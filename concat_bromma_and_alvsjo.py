8802cc59db24324013db94e3696f35d0f7b04d8b
Hannah Zemack <hannah.zemack@gmail.com>
2026-04-13 12:53:29 +0200

    concat vromma and alvsj0 data

---
+++ concat_bromma_and_alvsjo.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df1 = pd.read_csv('data_files/bromma_data_with_adress.csv')
+df2 = pd.read_csv('data_files/alvsjo_data_with_adress.csv')
+
+df_combined = pd.concat([df1, df2], ignore_index=True)
+df_combined.to_csv('data_files/combined_bromma_alvsjo_data_with_adresses.csv', index=False, encoding='utf-8-sig')
