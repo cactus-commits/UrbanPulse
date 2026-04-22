77cd78f9701e99e14e5502d6a9edba591403959c
Hannah Zemack <hannah.zemack@gmail.com>
2026-04-22 14:34:52 +0200

    add streamlit structure

--- stremlit_dashboard/dashboard.py
+++ stremlit_dashboard/dashboard.py
@@ -1,2 +1,11 @@
 import streamlit as st
 
+pg = st.navigation([
+    st.Page("first_page.py", title="Översikt", default=True),
+    st.Page("demografi.py", title="Demografi"),
+    st.Page("skolor.py", title="Skolor"),
+    st.Page("brottsstatistik.py", title="Brottsstatistik"),
+    st.Page("serviceutbud.py", title="Serviceutbud"),
+])
+
+pg.run()
