c7ba522d45689197cc2ebbb0b6ba69eff7ff2218
CHRISTOFFER CARLSSON <CHRISTOFFER.CARLSSON88@gmail.com>
2026-04-28 11:50:41 +0200

    edit: adjusted kpis.py for correct filtering. Mob prog Christoffer Hannah Miranda

--- bokoll/src/bokoll/components/kpis.py
+++ bokoll/src/bokoll/components/kpis.py
@@ -1,25 +1,24 @@
 import streamlit as st
 import duckdb
-from bokoll.utils.helpers import load_map_data
 from bokoll.utils.helpers import load_folkmangd
 
 
 def total_boende_kpi(vald_stadsdel='Alla', vald_stadsdelsomrade='Alla'):
     df = load_folkmangd()
 
+    # Filtrera bort summa-rader och rader utan stadsdelsområde
+    df = df[~df['Alder'].isin(['Total', 'No filters applied'])]
+    df = df.dropna(subset=['Alder', 'value', 'stadsdelsomrade'])
+
     if vald_stadsdel != 'Alla':
-        df = df[df['stadsdel'] == vald_stadsdel]
+        df = df[df['Stadsdel'] == vald_stadsdel]
     if vald_stadsdelsomrade != 'Alla':
         df = df[df['stadsdelsomrade'] == vald_stadsdelsomrade]
 
-    result = (
-        duckdb.sql("""
-            SELECT 
-                SUM("Befolkning Totalt Område"):: INT AS total_boende
-            FROM df
-        """)
-        .df()
-    )
+    result = duckdb.sql("""
+        SELECT SUM(value)::INT AS total_boende
+        FROM df
+    """).df()
 
-    st.metric(label="Totalt antal boende", value=result["total_boende"])
-
+    total = int(result["total_boende"].iloc[0])
+    st.metric(label="Totalt antal boende", value=f"{total:,}".replace(",", " "))
