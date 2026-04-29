0a1dbcd88fbbc58644d80a3fe3923f6fa22891b5
Hannah Zemack <hannah.zemack@gmail.com>
2026-04-29 12:28:49 +0200

    add line chart for demografi

--- bokoll/src/bokoll/pages/demografi.py
+++ bokoll/src/bokoll/pages/demografi.py
@@ -1,8 +1,19 @@
+from bokoll.components.filter import filter_layout
 import streamlit as st
+from bokoll.components.line_chart import line_chart_hyresutveckling
 
 def page_layout():
     st.title("Demografi")
     st.header("Filtrera på kategori och stadsdel")
+    filter_df = filter_layout()
+
+    col1, col2 = st.columns(2, gap="medium", vertical_alignment="center")
+
+    with col1:
+        st.subheader("Hyresutveckling")
+        with st.container(border=True):
+            line_chart_hyresutveckling(
+                vald_stadsdelsomrade=st.session_state.vald_stadsdelsomrade)
 
 
 if __name__ == "__main__":
