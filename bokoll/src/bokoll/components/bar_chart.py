8e8ea2b0736aec42312c00286de9b2ff43ceaf6f
Hannah Zemack <hannah.zemack@gmail.com>
2026-05-04 15:27:26 +0200

    change bar

--- bokoll/src/bokoll/components/bar_chart.py
+++ bokoll/src/bokoll/components/bar_chart.py
@@ -6,45 +6,6 @@ import altair as alt
 from bokoll.utils.helpers import load_boende, load_brott_2025
 from bokoll.assets.style.style_bar import FARGER, bygg_bar
 
-
-# def bar_chart(vald_stadsdel="Alla", vald_stadsdelsomrade="Alla"):
-#     # Hämta boendedata
-#     df = load_boende()
-
-#     # Filtrera ned till valt område
-#     if vald_stadsdel != "Alla":
-#         df = df[df["Stadsdel"] == vald_stadsdel]
-#     if vald_stadsdelsomrade != "Alla":
-#         df = df[df["stadsdelsomrade"] == vald_stadsdelsomrade]
-
-#     # Räkna ihop totalsumma per upplåtelseform
-#     aggregerat = (
-#         df.groupby("Upplåtelseform_Stor", as_index=False)["value"]
-#         .sum()
-#         .sort_values("value", ascending=False)
-#     )
-
-#     # Visa meddelande om ingen data hittades
-#     if aggregerat.empty:
-#         st.info("Ingen boendedata för det valda urvalet.")
-#         return
-
-#     # Skapa stapeldiagrammet
-#     fig = px.bar(
-#         aggregerat,
-#         x="Upplåtelseform_Stor",
-#         y="value",
-#         color="Upplåtelseform_Stor",
-#         color_discrete_map=FARGER,
-#         height=350
-#     )
-
-#     # Lägg på all styling från style_bar
-#     fig = styla_bar(fig)
-
-#     # Visa diagrammet i Streamlit
-#     st.plotly_chart(fig, use_container_width=True)
-
 def bar_chart(vald_stadsdel="Alla", vald_stadsdelsomrade="Alla"):
     df = load_boende()
 
