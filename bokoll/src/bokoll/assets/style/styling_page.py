8f5a3bd43c40b8980548faf9617f9ac55fd791cb
Hannah Zemack <hannah.zemack@gmail.com>
2026-05-04 10:30:31 +0200

    stylinf ro containers with function

--- bokoll/src/bokoll/assets/style/styling_page.py
+++ bokoll/src/bokoll/assets/style/styling_page.py
@@ -1,1 +1,20 @@
 import streamlit as st
+
+def styled_container(bg_color: str = "#FEFEFE", radius: str = "16px", padding: str = "10px", border: bool = True, height=None):
+    
+    st.markdown(
+        f"""
+        <style>
+        div.st-emotion-cache-18kf3ut.e1rw0b1u4 > div {{
+            background-color: {bg_color} !important;
+            border-radius: {radius} !important;
+            padding: {padding} !important;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    if height is not None:
+        return st.container(border=border, height=height)
+    return st.container(border=border)
